9cc5205891b783d594903741d4296908a6128547
Suhaib saifan <75537968+suhaib018@users.noreply.github.com>
2022-04-24 11:33:48 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from versatileimagefield.fields import VersatileImageField, PPOIField
+
 
 # Create your models here.
 
@@ -11,6 +13,7 @@ class Faculty(models.Model):
 class Department(models.Model):
     faculty=models.ForeignKey(Faculty,on_delete=models.CASCADE)
     name=models.CharField(max_length=50)
+    office_place=models.CharField(max_length=50,default='ds')
 
     def __str__(self):
         return self.name
@@ -19,6 +22,18 @@ class Employee(models.Model):
     department_employees=models.ForeignKey(Department,on_delete=models.CASCADE)
     first_name=models.CharField(max_length=20)
     last_name=models.CharField(max_length=20)
+    
 
     def __str__(self):
         return self.first_name
+
+
+
+class Image(models.Model):
+    image = VersatileImageField(
+        'Image',
+        upload_to='images/',
+        ppoi_field='image_ppoi'
+    )
+    image_ppoi = PPOIField()
+
